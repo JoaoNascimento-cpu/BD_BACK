74dedd75dc4dd9ddd2f5c33cd419556b70457ef3
DanielJJunior <daniel.souza.jr16@gmail.com>
2021-09-20 14:38:37 -0300

    React native iniciado

--- BackEnd_WallFertas/WallFertas/WallFertas/Contexts/WallFertas_Context.cs
+++ BackEnd_WallFertas/WallFertas/WallFertas/Contexts/WallFertas_Context.cs
@@ -31,7 +31,7 @@ namespace WallFertas.Contexts
             {
                // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
             //optionsBuilder.UseSqlServer("Data Source=Daniel-PC\\SQLEXPRESS; Initial Catalog= WallFertas; user Id=sa; pwd=senai@132;");
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-GUILHERM\\SQLEXPRESS; Initial Catalog= WallFertas; user Id=sa; pwd=48630533803;");
+                optionsBuilder.UseSqlServer("Data Source=Daniel-PC\\SQLEXPRESS; Initial Catalog= WallFertas; user Id=sa; pwd=senai@132;");
             }
         }
 
